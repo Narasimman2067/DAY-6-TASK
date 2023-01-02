ee13c92f8b1600e62f1a147c60c4624222dbe171
Narasimman2067 <narasimmamech2067@gmail.com>
2023-01-02 15:05:52 +0530

    task person details

---
+++ task person all details/script.js
@@ -0,0 +1,17 @@
+class Person{
+    constructor(Name,Age,Gender,Work,Salary,)
+    {
+        this.Name=Name;
+        this.Age=Age;
+        this.Gender=Gender;
+        this.Work=Work;
+        this.Salary=Salary
+        
+    }
+}
+var details=new Person("Narasimman","25","full stack develeoper","45000","Male")
+ console.log(details.Name)
+ console.log(details.Age)
+ console.log(details.Gender)
+ console.log(details.Work)
+ console.log(details.Salary)
